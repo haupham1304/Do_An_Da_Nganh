ead39f84de1b1924c71290b4cfbb54c3fb6dee7e
HoangproIT <phamhoang20092000@gmail.com>
2022-05-07 08:10:52 +0700

    commit fix API

--- API/api.py
+++ API/api.py
@@ -10,7 +10,7 @@ def home():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
 @app.route('/control', methods=['POST'])
-def home():
+def home1():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
 app.run()
