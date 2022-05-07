1841ee51804c1001ad167ecc2229426976642eaf
HoangproIT <phamhoang20092000@gmail.com>
2022-05-07 09:32:27 +0700

    get Data

--- API/api.py
+++ API/api.py
@@ -1,6 +1,8 @@
 from tkinter import Checkbutton
 import flask
 from tokenize import group
+import requests
+import json
 
 
 app = flask.Flask(__name__)
@@ -10,7 +12,21 @@ def CheckButton():
     pass
 
 def getData():
-    pass
+    dic={}
+    try:
+        responseNhietDo = requests.get("https://io.adafruit.com/api/v2/hoangproIT/feeds/nhiet-do/data")
+        responseDoAm = requests.get("https://io.adafruit.com/api/v2/hoangproIT/feeds/do-am/data")
+        lst_temp=json.loads(responseNhietDo.text.strip())
+        lst_humi=json.loads(responseDoAm.text.strip())
+        dic['nhiet_do']=[]
+        dic['do_am']=[]
+        for i in lst_temp:
+            dic['nhiet_do']+=[(i["created_at"],i["value"])]
+        for i in lst_humi:
+            dic['do_am']+=[(i["created_at"],i["value"])]
+    except:
+        pass
+    return dic
 
 @app.route('/check', methods=['GET'])
 def home():
